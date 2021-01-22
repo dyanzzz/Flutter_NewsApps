a4097ce1aee3aa993936a5613e566209e60740db
dian-tri-budianto <dian.budianto@berca.co.id>
2021-01-23 05:41:18 +0700

    init news apps by dicoding

---
+++ lib/ui/article_web_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:dicoding_news_app/widgets/custom_scaffold.dart';
+
+class ArticleWebView extends StatelessWidget {
+  static const routeName = '/article_web';
+  final String url;
+  const ArticleWebView({@required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: WebView(
+        initialUrl: url,
+      ),
+    );
+  }
+}
